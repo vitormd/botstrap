b673fa6e92d585b712e65c6def9c26ed28670e24
Vitor D'Amico <vitordami@gmail.com>
2017-04-29 00:47:39 -0300

    Remove chat_bots domain from foo_bot

---
+++ app/chat_bots/telegram/foo_bot.rb
@@ -0,0 +1,23 @@
+module Telegram
+  class FooBot < ChatBotBase
+    def update(update)
+      message = update[:message]
+      chat_id = update[:message][:chat][:id]
+
+      case update[:message][:text]
+      when 'Hi'
+        @api.sendMessage(
+          chat: chat_id,
+          text: 'Hello there!',
+          reply_to_message_id: message[:id]
+        )
+      else
+        @api.sendMessage(
+          chat: chat_id,
+          text: "Oops, I didn't understand that",
+          reply_to_message_id: message[:id]
+        )
+      end
+    end
+  end
+end
